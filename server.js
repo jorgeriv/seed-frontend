b3602aefde49e24e0b22cf3b53680a420cccfef8
Jorge Rivera <jorgeriv@gmail.com>
2016-05-22 11:12:32 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+const finalhandler = require('finalhandler');
+const http = require('http');
+const serveStatic = require('serve-static');
+const PORT = 8080;
+
+
+// Serve up public/ftp folder
+let serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']});
+
+// Create server
+let server = http.createServer(function(req, res){
+  let done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+// Listen
+server.listen(PORT);
+
+console.log('Static file server running at\n  => http://localhost:' + PORT + '/\nCTRL + C to shutdown');
